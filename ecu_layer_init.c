f075024088a5d2a61c73ca674b9afe0486d43b00
asem905 <123882184+asem905@users.noreply.github.com>
2024-06-28 15:39:56 +0300

    Update ecu_layer_init.c

--- ecu_layer_init.c
+++ ecu_layer_init.c
@@ -6,42 +6,43 @@
  */
 #include "ecu_layer_init.h"
 //#define _XTAL_FREQ 4000000UL
-//keypad_t keypad1={
-//    .keypad_row_pins[0].port=portc_index,
-//    .keypad_row_pins[0].direction=gpio_Direction_output,
-//    .keypad_row_pins[0].pin=pin0,
-//    .keypad_row_pins[0].logic=low,
-//    .keypad_row_pins[1].port=portc_index,
-//    .keypad_row_pins[1].direction=gpio_Direction_output,
-//    .keypad_row_pins[1].pin=pin1,
-//    .keypad_row_pins[1].logic=low,
-//    .keypad_row_pins[2].port=portc_index,
-//    .keypad_row_pins[2].direction=gpio_Direction_output,
-//    .keypad_row_pins[2].pin=pin2,
-//    .keypad_row_pins[2].logic=low,
-//    .keypad_row_pins[3].port=portc_index,
-//    .keypad_row_pins[3].direction=gpio_Direction_output,
-//    .keypad_row_pins[3].pin=pin3,
-//    .keypad_row_pins[3].logic=low,
-//    .keypad_column_pins[0].port=portc_index,
-//    .keypad_column_pins[0].direction=gpio_Direction_input,
-//    .keypad_column_pins[0].pin=pin4,
-//    .keypad_column_pins[0].logic=low,
-//    .keypad_column_pins[1].port=portc_index,
-//    .keypad_column_pins[1].direction=gpio_Direction_input,
-//    .keypad_column_pins[1].pin=pin5,
-//    .keypad_column_pins[1].logic=low,
-//    .keypad_column_pins[2].port=portc_index,
-//    .keypad_column_pins[2].direction=gpio_Direction_input,
-//    .keypad_column_pins[2].pin=pin6,
-//    .keypad_column_pins[2].logic=low,
-//    .keypad_column_pins[3].port=portc_index,
-//    .keypad_column_pins[3].direction=gpio_Direction_input,
-//    .keypad_column_pins[3].pin=pin7,
-//    .keypad_column_pins[3].logic=low
-//};
+/*=========================this code used for initialization part of keybad and LCD======================*/
+keypad_t keypad1={
+   .keypad_row_pins[0].port=portc_index,
+   .keypad_row_pins[0].direction=gpio_Direction_output,
+   .keypad_row_pins[0].pin=pin0,
+   .keypad_row_pins[0].logic=low,
+   .keypad_row_pins[1].port=portc_index,
+   .keypad_row_pins[1].direction=gpio_Direction_output,
+   .keypad_row_pins[1].pin=pin1,
+   .keypad_row_pins[1].logic=low,
+   .keypad_row_pins[2].port=portc_index,
+   .keypad_row_pins[2].direction=gpio_Direction_output,
+   .keypad_row_pins[2].pin=pin2,
+   .keypad_row_pins[2].logic=low,
+   .keypad_row_pins[3].port=portc_index,
+   .keypad_row_pins[3].direction=gpio_Direction_output,
+   .keypad_row_pins[3].pin=pin3,
+   .keypad_row_pins[3].logic=low,
+   .keypad_column_pins[0].port=portc_index,
+   .keypad_column_pins[0].direction=gpio_Direction_input,
+   .keypad_column_pins[0].pin=pin4,
+   .keypad_column_pins[0].logic=low,
+   .keypad_column_pins[1].port=portc_index,
+   .keypad_column_pins[1].direction=gpio_Direction_input,
+   .keypad_column_pins[1].pin=pin5,
+   .keypad_column_pins[1].logic=low,
+   .keypad_column_pins[2].port=portc_index,
+   .keypad_column_pins[2].direction=gpio_Direction_input,
+   .keypad_column_pins[2].pin=pin6,
+   .keypad_column_pins[2].logic=low,
+   .keypad_column_pins[3].port=portc_index,
+   .keypad_column_pins[3].direction=gpio_Direction_input,
+   .keypad_column_pins[3].pin=pin7,
+   .keypad_column_pins[3].logic=low
+};
 
-//led_t led10={.port_name=portd_index,.pin=pin0,.led_status=high};
+
 
 chr_lcd_4bit_t lcd_1={
     .lcd_rs.port=portc_index,
